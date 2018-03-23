3e183365f2e4708ee4cc9789bcebb582a7ea3d7c
ellisz.tamas <thomas.ellis@ebc.uu.se>
2018-03-23 17:51:55 +0100

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='faps',
-      version='1.2.2',
+      version='1.2.3',
       description=' Inference of paternity and sibling relationships accounting for uncertainty in genealogy',
       url='http://github.com/ellisztamas/faps',
       author='Tom Ellis',
