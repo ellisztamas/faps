7409e1b599f9b4595cd97c9af315e3d63684a6ae
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2020-04-14 16:17:32 +0200

    Changes to i/o for paternityArray objects. Updated to version 2.1.0.
    Firstly, read_paternity_array changed to work through pandas dataframes, which is much faster than numpy.genfromtxt. Removed the possibility to have a column of fathers.
    
    Secondly, add subset and split functions to paternityArray objects.

--- faps/read_paternity_array.py
+++ faps/read_paternity_array.py
@@ -1,69 +1,78 @@
 import numpy as np
 from faps.paternityArray import paternityArray
+from pandas import read_csv
 
-def read_paternity_array(path, likelihood_col = 2, mothers_col=1, fathers_col=None, delimiter=","):
+def read_paternity_array(path, mothers_col=1, likelihood_col = 2, fathers_col=None, delimiter=","):
     """
     Import a text file containing of log likelihoods of paternity.
 
-    If the data are for offspring individuals, optional columns can be used to
-    indicate their labels to match up with a file of parental data. Otherwise,
-    names for mothers and fathers are given as 'NA' in the output.
+    This is a shortcut to save having to run paternity_array every time you open
+    a data set. Note that only information on the IDs of the offspring and
+    mothers, likelihoods of paternity for each candidate and likelihoods that
+    fathers are missing can be retrieved; other information; for example
+    covariates must be generated again.
 
     Parameters
     ----------
     path: str
         filename path to locate the text file. This should contain unique ID
-        labels for each individual in the first column, followed by (optional)
-        columns of names for the mothers and fathers. After this include log
-        likelihoods that each adult is the sire of each offspring. The header
-        row should include names for each candidate, or else be left blank. The
-        final column should be a vector of likelihoods that the true sire of
-        each offspring has not been sampled.
-    genotype_col: int
-        indicate the column index where genotype information begins.
+        labels for each offspring individual in the first column, followed by
+        (optional)columns of names for the mothers and fathers. After this
+        include log likelihoods that each adult is the sire of each offspring.
+        The header row should include names for each candidate, or else be left
+        blank. The final column should be a vector of likelihoods that the true
+        sire of each offspring has not been sampled.
     mothers_col: int
         If a column of maternal names has been included, indicate column index
-        here.
-    fathers_col: If a column of paternal names has been included, indicate column
-        index here.
-    delimiter The symbol used to separate values in the text file. Defaults to
+        here. This is usually in the second column, which in Python speak is
+        column 1.
+    likelihood_col: int
+        indicate the column index where genotype information begins. This is
+        usually in the third column, which in Python speak is column 2, unless
+        you have given a column for fathers.
+    delimiter: str
+        The symbol used to separate values in the text file. Defaults to
         commas.
 
     Returns:
     --------
-    A paternityArray object.
+    A paternityArray object with attributes for likehood of paternity,
+    likelihoods that father are missing, IDs for the offspring and mothers,
+    and a vector of NAs for the fathers. Other possible attributes of a
+    paternityArray are set to `None`.
+
+    Examples
+    --------
+    from faps import *
+    import numpy as np
+
+    # Generate a population of adults
+    allele_freqs = np.random.uniform(0.3,0.5,50)
+    adults = make_parents(20, allele_freqs)
+    # Mate the first adult to the next three.
+    mother = adults.subset(0)
+    progeny = make_sibships(adults, 0, [1,2,3], 5, 'x')
+    # Create paternityArray
+    patlik = paternity_array(progeny, mother, adults, mu=0.0013)
+
+    # Write the paternityArray object to disk
+    patlik.write("my_paternity_array.csv")
+    # Read it back in
+    patlik2 = read_paternity_array("my_paternity_array.csv")
+    # Split the array into paternal families
+    patlik2.split(by=progeny.fathers)
     """
-    # likelihoods of paternity, and off absent fathers.
-    likelihood = np.genfromtxt(path, dtype='float', delimiter=delimiter)[1:, likelihood_col: -1]
-    lik_absent = np.genfromtxt(path, dtype='float', delimiter=delimiter)[1:, -1]
+    # Import data frame.
+    df = read_csv(path, delimiter=delimiter)
 
-    # Import individual names
-    names = np.genfromtxt(path, dtype='str')
-    offspring = np.array([i.split(",")[0].replace('"', '') for i in names])
+    # Create a new paternityArray object.
+    pat_array = paternityArray(
+        likelihood = df[df.columns[likelihood_col:-1]].to_numpy(),
+        lik_absent = df[df.columns[-1]].to_numpy(),
+        offspring  = df[df.columns[0]].to_numpy(),
+        mothers    = df[df.columns[mothers_col]].to_numpy(),
+        fathers    = np.repeat('NA', len(df)),
+        candidates = df.columns[likelihood_col:-1]
+    )
 
-    # Names for the mothers, if these are given
-    if isinstance(mothers_col, int):
-        mothers = np.array([i.split(",")[mothers_col].replace('"', '') for i in names])
-    # If they aren't, return a list of NAs
-    if mothers_col is None:
-        mothers = np.repeat('NA', likelihood.shape[0])
-
-    # Names for the fathers, if given
-    if isinstance(fathers_col, int):
-        fathers = np.array([i.split(",")[fathers_col].replace('"', '') for i in names])
-    # If they aren't, return a list of NAs
-    if fathers_col is None:
-        fathers = np.repeat('NA', likelihood.shape[0])
-
-    # pull out names of candidate males
-    candidates = names[0].split(",")[likelihood_col: -1]
-
-    # Return a vector of zeroes for the covariates
-    covar = np.zeros(likelihood.shape[0])
-    return paternityArray(likelihood = likelihood,
-        lik_absent= lik_absent,
-        offspring = offspring,
-        mothers = mothers,
-        fathers = fathers,
-        candidates = candidates,
-        covariate = 0)
+    return pat_array
